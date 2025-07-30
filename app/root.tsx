1943a820d330947a922b7a9b94b911ce37464598
CyMonkeyz <Faza.p.wildan@gmail.com>
2025-07-30 10:48:42 +0700

    fix tailwind css import

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
-import "./output.css";
+import "./app.css";
 
 export default function Root() {
   return (
